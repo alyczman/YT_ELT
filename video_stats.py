8eaa388420c9e081d62dd528e9f9cfa28f9d78b3
Andrew Lyczmanenko <alyczman@gmail.com>
2026-03-25 21:40:22 -0400

    Added function to save API data to json file

--- video_stats.py
+++ video_stats.py
@@ -2,6 +2,7 @@ from dotenv import load_dotenv
 import requests
 import json
 import os
+from datetime import date
 
 
 load_dotenv(dotenv_path='./.env')
@@ -142,8 +143,20 @@ def extract_video_data(video_ids):
 
     except requests.exceptions.RequestException as e:
         raise e
+    
+
+def save_to_json(extracted_data):
+    file_path = f'./data/YT_data_{date.today()}.json'
+
+    # context manager (with)
+     # write, so set mode to w
+    with open(file_path, 'w', encoding='utf-8') as json_outfile:
+        json.dump(extracted_data, json_outfile, indent = 4, ensure_ascii=False)
+
+
+
 if __name__ == "__main__":
     playlistID = get_playlist_id()
     video_ids = get_video_ids(playlistID)
-    print(extract_video_data(video_ids))
-    
+    video_data = extract_video_data(video_ids)
+    save_to_json(video_data)
